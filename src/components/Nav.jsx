537dbe6a8677454e04dce227188326d5dc3a420e
MurtazaKhannn <152141459+MurtazaKhannn@users.noreply.github.com>
2024-07-08 21:27:46 +0530

    responsive 3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,12 @@ import { FaPhone } from 'react-icons/fa';
 const Nav = () => {
   return (
     <div className='flex h-[10vh] justify-between w-full items-center'>
-      <h1 className='text-4xl xs:text-lg ml-8 xs:ml-3 font-bold '>
+      <h1 className='text-4xl xs:text-4xl xs:items-center xs:w-full xs:pl-32 xs:justify-center ml-8 xs:ml-3 font-bold '>
         <i className='font-bebas text-red-700 flex'>
           Murtaza <p className='text-white'>.</p> <p className='text-blue-500'>Dev</p>
         </i>
       </h1>
-      <div className='text-white w-[60%] xs:w-[70%] xs:mr-5 xs:gap-3 justify-evenly flex items-center'>
+      <div className='text-white xs:hidden w-[60%] xs:w-[70%] xs:mr-5 xs:gap-3 justify-evenly flex items-center'>
         <Link to="home" smooth={true} duration={2000} className='hover:border-b-4 xs:hidden hover:border-yellow-400 py-2 rounded cursor-pointer'>Home</Link>
         <Link to="GameSection" smooth={true} duration={2000} className='hover:border-b-4 hover:border-yellow-400 py-2 rounded cursor-pointer'>GameSection</Link>
         <Link smooth={true} duration={2000} to="projects" className='hover:border-b-4 hover:border-yellow-400 py-2 rounded cursor-pointer'>Projects</Link>
